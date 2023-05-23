b855cd5684fb212b86b100b6167869a960e5e769
Niket Bachhawat <workspaceniket@gmail.com>
2023-05-23 06:20:58 +0530

    "Added new ejs file and Made Header and Footer Ready"

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
+const local = {
+  title: "Blog-Notes",
+  description: "Made using NodeJS Express & MongoDB",
+};
+
 router.get("/", (req, res) => {
-  res.status(200).send("Hello World");
+  // Passing data to ejs files
+  res.status(200).render("index", { local });
+});
+
+router.get("/about", (req, res) => {
+  res.status(200).render("about", { local });
 });
 
 module.exports = router;
